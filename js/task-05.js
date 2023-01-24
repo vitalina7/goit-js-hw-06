c99b0e4b0594387f6bfbaa2ab44da3e57822b22b
Buhal Vitalina <luvot@ukr.net>
2023-01-24 08:41:50 +0200

    виправила 5 завдання

--- js/task-05.js
+++ js/task-05.js
@@ -2,11 +2,9 @@ const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector('#name-output');
 inputName.addEventListener('input', changeName);
 function changeName(event) {
-    if (event.currentTarget.value !== "") {
-   outputName.textContent=event.currentTarget.value;
-    }
-    else {
+    outputName.textContent = event.currentTarget.value;
+    if (!event.currentTarget.value){
         outputName.textContent = "Anonymous";
-    }
-  
 }
+    }
+
