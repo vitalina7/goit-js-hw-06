b7aaf664f6cf7e0db1bd7dbfd7c6bf482fb28c9f
Buhal Vitalina <luvot@ukr.net>
2023-02-03 14:47:15 +0200

    edit exercise 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,12 @@ const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector('#name-output');
 inputName.addEventListener('input', changeName);
 function changeName(event) {
-    outputName.textContent = event.currentTarget.value;
-    if (!event.currentTarget.value){
+    if (event.currentTarget.value) {
+        outputName.textContent = event.currentTarget.value;
+        return;
+    }
+    
         outputName.textContent = "Anonymous";
 }
-    }
+    
 
